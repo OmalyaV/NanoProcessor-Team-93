c5d369eac33fbfe5a877c1cfcfc54c2762825499
OmalyaV <omalyavid@gmail.com>
2023-05-31 03:14:51 +0530

    Update Instruction_decoder.vhd

--- Lab9/Lab9.srcs/sources_1/new/Instruction_decoder.vhd
+++ Lab9/Lab9.srcs/sources_1/new/Instruction_decoder.vhd
@@ -32,12 +32,35 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Instruction_decoder is
-    Port ( Instruction_decoder_in : in STD_LOGIC_VECTOR (11 downto 0));
+    Port ( Instruction_decoder_in : in STD_LOGIC_VECTOR (11 downto 0);
+           register_check_for_jump: in STD_LOGIC_VECTOR(3 downto 0);
+           add_or_sub_select: out STD_LOGIC := '0';
+           register_enable: out STD_LOGIC_VECTOR(2 downto 0):= "000";
+           register_select : out STD_LOGIC_VECTOR(5 downto 0):= "00000";
+           immidiate_value: out STD_LOGIC_VECTOR(3 downto 0):= "0000";
+           load_select: out STD_LOGIC :='0';
+           jump_flag: out STD_LOGIC := '0';
+           address_to_jump:out STD_LOGIC_VECTOR(2 downto 0):= "000"
+           
+           
+           );
+    
 end Instruction_decoder;
 
 architecture Behavioral of Instruction_decoder is
 
 begin
 
+process(Instruction_decoder_in(11 downto 10))
+begin
+if (Instruction_decoder_in(11 downto 10)= "00") then
+    add_or_sub_select <= '1';
+    register_select(5 downto 3)<= Instruction_decoder_in(9 downto 7);
+    register_select(2 downto 0)<= Instruction_decoder_in(6 downto 4);
+elsif (Instruction_decoder_in(11 downto 10)= "10") then
+    
+end if;
+end process;
+    
 
 end Behavioral;
