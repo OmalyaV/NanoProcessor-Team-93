268168d48e7f5b80092521ad5b59459f7547dffe
OmalyaV <omalyavid@gmail.com>
2023-05-31 02:01:08 +0530

    files added

---
+++ Lab9/Lab9.srcs/sources_1/new/Instruction_decoder.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/31/2023 02:00:43 AM
+-- Design Name: 
+-- Module Name: Instruction_decoder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Instruction_decoder is
+    Port ( Instruction_decoder_in : in STD_LOGIC_VECTOR (11 downto 0));
+end Instruction_decoder;
+
+architecture Behavioral of Instruction_decoder is
+
+begin
+
+
+end Behavioral;
