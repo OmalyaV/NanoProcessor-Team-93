738c356b80d6c218ec851c57456a96d9138db654
OmalyaV <omalyavid@gmail.com>
2023-06-01 10:25:54 +0530

    Update Instruction_decoder.vhd
    modified

--- Lab9/Lab9.srcs/sources_1/new/Instruction_decoder.vhd
+++ Lab9/Lab9.srcs/sources_1/new/Instruction_decoder.vhd
@@ -48,39 +48,50 @@ entity Instruction_decoder is
 end Instruction_decoder;
 
 architecture Behavioral of Instruction_decoder is
+signal ins_code :STD_LOGIC_VECTOR(1 downto 0);
+signal reg_sel_A,reg_sel_B, reg_sel_A_ins, reg_sel_B_ins: STD_LOGIC_VECTOR(2 downto 0);
+signal reg_enable_ins, address_to_jump_ins: STD_LOGIC_VECTOR(2 downto 0);
+signal immidiate_value_ins: STD_LOGIC_VECTOR(3 downto 0);
+
+
 
 begin
 
-process(Instruction_decoder_in(11 downto 10))
+ins_code<= Instruction_decoder_in(11 downto 10);
+reg_sel_A_ins <= Instruction_decoder_in(9 downto 7); --to select the registrer A 
+reg_sel_B_ins <= Instruction_decoder_in(6 downto 4); --to select the register B
+reg_enable_ins<= Instruction_decoder_in(9 downto 7); --to enable a register in the register bank
+immidiate_value_ins <= Instruction_decoder_in(3 downto 0); -- immidiate vallue decoded from the instruction 
+address_to_jump_ins <= Instruction_decoder_in(2 downto 0);-- adress to jump when jump instruction is enable
+process(ins_code)
 begin
-if (Instruction_decoder_in(11 downto 10)= "00") then --ADD
+
+if (ins_code= "00") then --ADD
     
-    register_select(5 downto 3)<= Instruction_decoder_in(9 downto 7);
-    register_select(2 downto 0)<= Instruction_decoder_in(6 downto 4);
+    register_select(5 downto 3)<= reg_sel_A_ins;
+    register_select(2 downto 0)<= reg_sel_B_ins;
     add_or_sub_select <= '1';
-    register_enable <= Instruction_decoder_in(9 downto 7);
+    register_enable <= reg_enable_ins;
     load_select<= '1';
     
-elsif(Instruction_decoder_in(11 downto 10)= "01") then --NEG
+elsif(ins_code= "01") then --NEG
     
-    register_select(5 downto 3)<= Instruction_decoder_in(9 downto 7);
+    register_select(5 downto 3)<= reg_sel_A_ins;
     register_select(2 downto 0)<= (others => '0');
     add_or_sub_select <= '1';
-    register_enable <= Instruction_decoder_in(9 downto 7);
+    register_enable <= reg_enable_ins;
     load_select<= '1';
     
+elsif (ins_code= "10") then --MOVIN
+    immidiate_value<= immidiate_value_ins;
+    register_enable <= reg_enable_ins;
     
-    
-    
-elsif (Instruction_decoder_in(11 downto 10)= "10") then --MOVIN
-    immidiate_value<= Instruction_decoder_in(3 downto 0);
-    register_enable <= Instruction_decoder_in(9 downto 7);
-else --JZR
-    register_select(5 downto 3) <= Instruction_decoder_in(9 downto 7);
+elsif (ins_code ="11") then --JZR
+    register_select(5 downto 3) <= reg_sel_A_ins;
     if(register_check_for_jump ="0000") then
-            address_to_jump <= Instruction_decoder_in(2 downto 0);
+            address_to_jump <= address_to_jump_ins;
             jump_flag <= '1';
-        end if;
+    end if;
     
 end if;
 end process;
